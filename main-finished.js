071821b2c0fb04cdfd6f562ba81e7d17ef63f81e
bedribulut <96528707+bedribulut@users.noreply.github.com>
2022-08-06 19:10:38 +0300

    Update main-finished.js

--- main-finished.js
+++ main-finished.js
@@ -85,7 +85,7 @@ class EvilCircle extends Shape {
 
    constructor(x, y) {
       super(x, y, 20, 20);
-      this.color = white;
+      this.color = 'white';
       this.size = 10;
 
       window.addEventListener('keydown', (e) => {
